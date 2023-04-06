07a39f0d41797c8067fe80f4fe5d06f29364b069
Константин <osokinkonstantin@mail.ru>
2023-04-06 21:06:57 +0300

    задачи 4-5

--- src/main.java
+++ src/main.java
@@ -11,7 +11,14 @@ public class main {
         System.out.println(dog - 3.5);
         System.out.println(cat - 1.6);
         System.out.println(paper - 7639);
+        var friends = 19;
+        friends = friends +2;
+        System.out.println(friends / 2);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
-
-
