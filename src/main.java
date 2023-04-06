021fc55691d60dfce18c7016e60f9ac94794599e
Константин <osokinkonstantin@mail.ru>
2023-04-06 22:01:01 +0300

    задачи 6-8

--- src/main.java
+++ src/main.java
@@ -20,5 +20,23 @@ public class main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+
+        var weightBoxer1 = 78.2;
+        var weightBoxer2 = 82.7;
+        var totalWeightBoxers = weightBoxer1 + weightBoxer2;
+        System.out.println(totalWeightBoxers);
+        var weightDifference1 = weightBoxer2 - weightBoxer1;
+        System.out.println("разница в вессе" + weightDifference1 + " кг");
+        var weightDifference2 = weightBoxer2 % weightBoxer1;
+        System.out.println("разница в вессе" + weightDifference2 + " кг!!!!!!!!");
+
+        var hour1 = 640;
+        var employee1 = hour1 / 8;
+        System.out.println("количество сотрудников" + employee1);
+        var newEmployee = employee1 + 94;
+        var newHours = newEmployee * 8;
+        System.out.println("если в компании работает " + newEmployee +" сотрудника, то всего " + newHours + " часа может быть поделено между сотрудниками");
+
+
     }
 }
