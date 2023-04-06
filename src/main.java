0a77bef191a913b53d22fca78d82b59a5feb49d1
Константин <osokinkonstantin@mail.ru>
2023-04-06 20:50:40 +0300

    задачи 1-3

---
+++ src/main.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class main {
+    public static void main(String[] args) {
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763_789;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog - 3.5);
+        System.out.println(cat - 1.6);
+        System.out.println(paper - 7639);
+    }
+}
+
+
